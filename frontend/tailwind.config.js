7fe47f5b835f70abacf966ae65f56fc4751dd0da
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-27 12:55:39 +0530

    initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: false,
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
+};
